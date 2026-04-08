288b596606ffab7dba2ea7299d052333d13c2b29
Ryan Atkinson <mail@ryanatkn.com>
2026-04-08 19:49:37 -0400

    wip

--- gro.config.ts
+++ gro.config.ts
@@ -31,7 +31,6 @@ const config: CreateGroConfig = async (base_config) => {
 				flags: [
 					'--no-check',
 					'--sloppy-imports',
-					'--allow-ffi',
 					'--include',
 					'node_modules/@fuzdev/blake3_wasm',
 				],
